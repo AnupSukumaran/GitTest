a186fa94adeb13543aaf962001c1d1f6977fc104
Anup Sukumaran <anup.sukumaran9@gmail.com>
2019-06-10 15:10:26 +0530

    added MapForItinerary

---
+++ GitTest/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitTest
+//
+//  Created by Qaptive Technologies on 10/06/19.
+//  Copyright © 2019 Qaptive Technologies. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
